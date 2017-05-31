d5ae02c4ffdf8b7459533b314d6eb059cc949197
guang ygg <guang ygg>
2017-05-31 22:09:33 +0800

    20150531

--- public/js/detail/comment.js
+++ public/js/detail/comment.js
@@ -1,23 +1,28 @@
-$("#submit").click(function(e){
-	e.preventDefault();
-	var comment_cont = $("#comment_box").val();
+if(SAL_cookie.check("username")){
+	$("#submit").click(function(e){
+		var comment_cont = $("#comment_box").val();
+		var users_cont_box = $("#users");
+		var username = SAL_cookie.read("username");
 
-	$.ajax({
-		url: '/detail/comment',
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			detailMark: 'value1',
-			
-			commentCont:comment_cont
-		},
-	})
-	.done(function(data) {
-		console.log("success");
-	})
-	.fail(function(err) {
-		console.error(err);
+		e.preventDefault();
+		$.ajax({
+			url: '/detail/comment',
+			type: 'POST',
+			data: {
+				detailMark : window.detailID,
+				username   : username,
+				commentCont: comment_cont
+			},
+		})
+		.done(function(data) {
+			if(data === "success"){
+				users_cont_box.prepend('<li><div class="user">'+ username +' </div><div class="user-cont"> ' + comment_cont + ' </div></li>');
+			}
+		})
+		.fail(function(err) {
+			console.error(err);
+		});
 	});
-});
+};
 
 	
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,29 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-var fs = require("fs");
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false }); 
-var userinfo = require("./../data-storage/userinfo.json");
-//var cookieParser = require('cookie-parser');
+let express = require('express');
+let router = express.Router();
+let path = require('path');
+let fs = require("fs");
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let userinfo = require("./../data-storage/userinfo.json");
+//let cookieParser = require('cookie-parser');
 
 //tools
-Array.prototype.contains = function(el){
-    for (index in this){
-        if( this[index] === el) return  {"contain":true,"location":index} ;
-    };
-    return {contains:false,location:null} ;
+function Utility(){};
+Utility.prototype = {
+    isArray : function(obj){
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    },
+    contain : function(arr,el){
+        if( !this.isArray(arr) ) return console.error("The first parameter does not belong to Array!");
+        for (index in arr){
+            if( arr[index] === el) return  {"contain":true,"location":index} ;
+        };
+        return {contain:false,location:null} ;
+    }
 };
+let utility = new Utility();
+
+function fillZero(variable){return variable < 10 ?  '0'+ variable : String(variable)};
 
 //homepage
 router.get('/', function(req, res) {
@@ -21,13 +31,40 @@ router.get('/', function(req, res) {
 });
 
 //detail
-router.get('/detail/01', function(req, res) {
-  res.render('detail/01');
-});
+let detail_legnth = 2 ;
+for(let j = 1 ; j <= detail_legnth ;j++){
+    j = fillZero(j);
+
+    router.get('/detail/' + j, function(req, res) {
+        res.render('detail/' + j, { detailID : j });
+    });
+};
+
 
 //comment
-router.post('/detail/comment', function(req, res) {
-  
+router.post('/detail/comment', urlencodedParser,function(req, res) {
+    let detailMark = req.body.detailMark;
+    let username = req.body.username;
+    let commentCont = req.body.commentCont;
+    let new_comment = {
+        "username" : username,
+        "comment"  : commentCont
+    };
+    console.log(new_comment);
+    if( detailMark && username && commentCont ){
+        fs.readFile("./data-storage/detail_data.json","utf8",function(error,data){
+            if(error) return console.error(error);
+            
+            let detail_data = JSON.parse(data);
+
+            detail_data[detailMark]["comments"].unshift(new_comment);
+
+            fs.writeFile("./data-storage/detail_data.json",JSON.stringify(detail_data),function(error){
+                if(error) return console.error(error);
+                res.send("success");
+            });
+        });
+    };
 });
 
 //entry
@@ -36,28 +73,28 @@ router.get('/entry', function(req, res) {
 });
 
 router.post('/entry/ent',urlencodedParser, function(req, res) {
-	if (!req.body) return res.sendStatus(400);  
- 
-    var username = req.body.username;
-    var password = req.body.password;	
+	if (!req.body) return res.sendStatus(400);
+
+    let username = req.body.username;
+    let password = req.body.password;
 
     fs.readFile("./data-storage/userinfo.json","utf8",function (error,data){
-     	if(error) return console.error(error); 
+     	if(error) return console.error(error);
 
-     	var userinfo_data = JSON.parse(data);
-     	var username_set = [];
-     	var username_val_set = [];
-     	
+     	let userinfo_data = JSON.parse(data);
+     	let username_set = [];
+     	let username_val_set = [];
+
         //check username
-     	for(var i = 0 ; i < userinfo_data.length ; i++){
+     	for(let i = 0 ; i < userinfo_data.length ; i++){
      		username_set.push(userinfo_data[i].username);
             username_val_set.push(userinfo_data[i].pwd);
      	};
 
-        if(username_set.contains(username)["contain"]){
-            if(username_val_set[username_set.contains(username)["location"]] === password){
-                console.log("登录成功！");
+        if(utility.contain(username_set,username)["contain"]){
+            if(username_val_set[utility.contain(username_set,username)["location"]] === password){
                 //登录成功
+                console.log("登录成功！");
                 res.cookie("username",username,{ maxAge:7*24*60*60*1000, path:"/"});
                 res.redirect('/');
             }else{
@@ -79,36 +116,36 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register/reg', urlencodedParser,function(req, res) {
-	if (!req.body) return res.sendStatus(400);  
- 
-    var username = req.body.username;
-    var password = req.body.password;
+	if (!req.body) return res.sendStatus(400);
+
+    let username = req.body.username;
+    let password = req.body.password;
 
   	fs.readFile("./data-storage/userinfo.json","utf8",function (error,data){
-     	if(error) return console.error(error); 
+     	if(error) return console.error(error);
 
-     	var userinfo_data = JSON.parse(data);
-     	var username_set = [];
+     	let userinfo_data = JSON.parse(data);
+     	let username_set = [];
      	//check username
-     	for(var i = 0 ; i < userinfo_data.length ; i++){
+     	for(let i = 0 ; i < userinfo_data.length ; i++){
      		username_set.push(userinfo_data[i].username);
      	};
-     	if(username_set.contains(username)["contain"]){
+
+     	if(utility.contain(username_set,username)["contain"]){
      		res.render('register/register');
      	}else{
 	     	//write userinfo.json
 	     	userinfo_data.push({"username":username,"pwd":password});
 	     	fs.writeFile("./data-storage/userinfo.json",JSON.stringify(userinfo_data),function(error){
 	     		if(error) return console.error(error);
-	     	});	
-
-	     	//res.render('index/index',{"is_login":true,"username":username});
-	     	res.cookie("username",username,{ maxAge:7*24*60*60*1000, path:"/"});
-	     	res.redirect('/')
+                //res.render('index/index',{"is_login":true,"username":username});
+                res.cookie("username",username,{ maxAge:7*24*60*60*1000, path:"/"});
+                res.redirect('/')
+	     	});
      	};
 	});
 
-       
+
 });
 
 module.exports = router;
